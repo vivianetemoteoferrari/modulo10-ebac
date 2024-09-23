2114b081e2f1e59058bafcf8a0122648dd88022c
Viviane Ferrari <viviane.tferrari@gmail.com>
2024-09-23 13:27:43 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    $('#carousel-imagens').slick({
+        autoplay: true,
+    });
+
+    $('.menu-hamburguer').click(function () {
+        $('nav').slideToggle();
+    });
+
+    $('#telefone').mask('(00) 00000-0000');
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+            veiculoDeInteresse: {
+                required: false
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira seu nome.',
+            email: 'Por favor, insira um endereço de e-mail válido.',
+            telefone: 'Por favor, insira seu telefone.',
+            mensagem: 'Por favor, insira uma mensagem.'
+        },
+        submitHandler: function(form) {
+            console.log('Formulário enviado');
+            form.submit();  
+        },
+        invalidHandler: function (e, validator) {
+            let camposIncorretos = validator.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`);
+            }
+        }
+    });
+
+    $('.lista-veiculos button').click(function () {
+        const destino = $('#contato');
+        const nomeVeiculo = $(this).parent().find('h3').text();
+
+        $('#veiculo-interesse').val(nomeVeiculo);
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+
+    })
+});
